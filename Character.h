3c3b3172cdd04f8b717df43ba96cf3c122015e28
Frank474747 <stst.100099@gmail.com>
2024-11-17 23:46:57 +0800

    建立Character雛形

--- Character.h
+++ Character.h
@@ -2,13 +2,29 @@
 #define CHARACTER_H
 
 #include <string>
+#include <vector>
+#include <Stock.h>
 using namespace std;
 
 class Character{
 protected:
+    // 身分相關
+    short id;
+    string identity; // 或直接用數字表示身分
     string name;
-    int initCurrencyBonus;
-    int totalAsset;
+    string description;
+
+    // 功能相關
+    int initBonus; // 可能設計成高基礎值多少
+    int currentMoney;
+    vector<Stock> stocks;
+
+public:
+    int getCurrentMoney();
+    string getName();
+    // get identity
+    string getDesciption();
+    int getTotalAsset(); // currentMoney + Σ(s.num * s.price) for s in stocks
 };
 
 #endif // CHARACTER_H

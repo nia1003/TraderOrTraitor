411ca611fac87a78cf1592d225b3c4f3c11ce3c1
Maurice <maurice8huang@gmail.com>
2024-11-20 20:17:29 +0800

    完成event類別，incident改名叫改名叫Event

---
+++ Event.h
@@ -0,0 +1,23 @@
+#ifndef incident.h
+#define incident.h
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Incident{
+private:
+    vector<double> affectedStocks;
+    string description;
+    int eventImpact;
+    string source;
+public:
+    /*
+    influeneStockPrice
+    
+    */
+
+};
+
+#endif
--- stock.h
+++ stock.h
@@ -4,21 +4,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
-
+using namespace std;
 
 class Stock{
 private:
     int stockID; // 股票的代碼
-    std::string name; // 股票名稱
-    std::string industry; // 個股事件、產業事件、總體事件
+    string name; // 股票名稱
+    string industry; // 個股事件、產業事件、總體事件
     double current_price; // 當前價格
-    std::string description; // 股票描述
+    string description; // 股票描述
     double volatility; // 股票波動率
     double sensitivity; // 事件敏感度
-    std::vector<double> priceHistory; // 股票的歷史價格數據，回合結束時更新
+    vector<double> priceHistory; // 股票的歷史價格數據，回合結束時更新
 
 public:
-    Stock(int id, const std::string& name, const std::string& industry, double price, const std::string& description, double volatility, double sensitivity);
+    Stock(int id, const string& name, const string& industry, double price, const string& description, double volatility, double sensitivity);
 
     ~Stock();
 

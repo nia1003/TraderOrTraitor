9b2b885757dba844e435e632d6163e076e362670
Frank474747 <stst.100099@gmail.com>
2024-11-26 01:25:32 +0800

    簡單測試流程用，尚未製作

---
+++ Round.h
@@ -0,0 +1,23 @@
+#ifndef ROUND_H
+#define ROUND_H
+
+#include "Character.h"
+#include <unordered_map>
+using namespace std;/*
+    section1: show events
+    section2: players trade (and finance, release news)
+    section3: show results after the operaions
+*/
+
+class Round {
+private: // for testing
+public:
+
+};
+
+class Stage {
+private:
+    unordered_map<int, Character*> characters;
+};
+
+#endif

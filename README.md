0de5da2be1d649f576d52f5dbaeff64cae86ab8f
Maurice <maurice8huang@gmail.com>
2024-12-03 11:01:44 +0800

    把member function放在.cpp檔案中

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TraderOrTraitor
+## 黃子寰 部分
+- 事件物件儲存在src file中"test_event.cpp"的檔案中
+- 股票物件儲存在src file中"test_stock.cpp"的檔案中
--- src/stock.h
+++ src/stock.h
@@ -42,34 +42,5 @@ public:
    void printStockInfo() const;
 
 };
-// ---成員函數---
-void Stock::printStockInfo() const
-{
-    cout << "股票資訊:" << endl;
-    cout << "------------------" << endl;
-    cout << "股票ID: " << ticker << endl;
-    cout << "股票名稱: " << name << endl;
-    cout << "產業類別: " << industry << endl;
-    cout << "目前價格: $" << current_price << endl;
-    cout << "個股描述: " << description << endl;
-    cout << "事件敏感度: " << sensitivity << endl;
-
-    if (!priceHistory.empty()) {
-        cout << "價格歷史: ";
-        for (size_t i = 0; i < priceHistory.size(); ++i) {
-            cout << priceHistory[i];
-            if (i != priceHistory.size() - 1) cout << ", ";
-        }
-        cout << endl;
-    } else {
-        cout << "Price History: No data available yet." << endl;
-    }
-
-    cout << "------------------" << endl;
-}
-void Stock::addToPriceHistory(int current_price)
-{
-    priceHistory.push_back(current_price);
-}
 
 #endif
